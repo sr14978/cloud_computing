939f871fb4d4fd3d6b2b42dc6cc74ea1b47ae337
Samuel Russell <sr14978@bristol.ac.uk>
2017-12-30 13:17:30 +0000

    fixed bugs

--- breakup.py
+++ breakup.py
@@ -1,17 +1,17 @@
 from flask import Flask, Blueprint, request
-
+import json
 breakup = Blueprint('breakup_blueprint', __name__)
     
 messages = []
-@breakup.route("/")
+@breakup.route("/", methods=['POST'])
 def job():
     data = json.loads(request.data.decode('utf-8'))
     messages.append(data)
-    return 200
+    return 'Ok', 200
 
 @breakup.route("/get")
 def get():
-    return messages.append(data).encode('utf-8')
+    return str(messages)
     
 app = Flask(__name__)
 app.register_blueprint(breakup, url_prefix='/_ah/breakup')

e8499fe9ce90722b87f2eee8ee03b2a024cf9f83
Samuel Russell <sr14978@bristol.ac.uk>
2017-12-30 01:59:51 +0000

    index.js

--- main/static/scripts/index.js
+++ main/static/scripts/index.js
@@ -1,7 +1,7 @@
 "use strict"
 window.addEventListener("load", onLoad);
 
-function onLoad(
+function onLoad(Event e) {
   getElementById("fileUpload-form").addEventListener("submit", onSubmitForm);
 
   function onSubmitForm(Event e) {

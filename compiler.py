d45d6cb5cfb29f398d6f063a7f966eca13ad883c
Jamie Willis <jw14896@my.bristol.ac.uk>
2017-12-31 15:05:47 +0000

    Should be almost good :)

--- compiler.py
+++ compiler.py
@@ -1,19 +1,23 @@
 import subprocess
+import pickle
+
+def process(msgstr):
+    return [msgstr]
 
 class Success:
-    def __init__(self, filename, warnings):
-        self.filename = filename
-        self.warnings = warnings
-        
+    def __init__(self, warnings):
+        self.warnings = process(warnings)
+
 class Error:
-    def __init__(self, filename, msgs):
-        self.filename = filename
-        self.warnings = warnings
-        
+    def __init__(self, msgs):
+        self.warnings = process(warnings)
+
 def compile(source_path, object_path, compiler='g++', flags=''):
     print("compiling " + source_path)
     cargs = [compiler, "-c", source_path, "-o", object_path] + flags.split(" ")
+    # if this succeeds we wrap the result up in a Success object
     try:
-        return subprocess.check_output([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)
+        return pickle.dumps(Success(subprocess.check_output([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)))
+    # otherwise we return an Error object
     except subprocess.CalledProcessError as e:
-        return e.output
+        return pickle.dumps(Error(e.output))
--- linker.py
+++ linker.py
@@ -1,9 +1,37 @@
 import subprocess
+import compiler
+import pickles
+
+class Result:
+    def __add__(self, msgs):
+        self.msgs += msgs
+        return self
+
+    @classmethod
+    def process(cls, msgstr):
+        return [msgstr]
+
+class Success(Result):
+    def __init__(self, msgstr):
+        self.msgs = self.process(msgstr)
+
+class Failure(Result):
+    def __init__(self, msgstr):
+        self.msgs = self.process(msgstr)
 
 def link(msgs, get_object_files, exename, compiler, flags):
+    results = [pickle.loads(msg) for msg in msgs]
     objects = get_object_files()
-    cargs = [compiler, "-o", exename] + objects + flags.split(" ")
-    try:
-        return subprocess.check_output([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        return e.output
+    msgs = merge(results)
+    if all(map(lambda result: isinstance(result, compiler.Success), results)):
+        cargs = [compiler, "-o", exename] + objects + flags.split(" ")
+        try:
+            return Success(subprocess.check_output([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)) + msgs
+        except subprocess.CalledProcessError as e:
+            return Failure(e.output) + msgs
+    else:
+        return None
+
+def merge(results): #[String]
+    # results: List[compiler.Success | compiler.Failure]
+    return [msg for result in results for msg in result.msgs]

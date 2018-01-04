5d68ff1e72e2db2be5368b96a313485b652e1e51
Samuel Russell <sr14978@bristol.ac.uk>
2018-01-04 23:10:07 +0000

    Oauth

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask, redirect, render_template
 import database
+from oauth2client.contrib.flask_util import UserOAuth2
+
+oauth2 = UserOAuth2()
+
 app = Flask(__name__)
+
+oauth2.init_app(
+        app,
+        scopes=['email', 'profile'],
+        authorize_callback=_request_user_info)
+
 @app.route("/")
 def index():
     return redirect('/static/index.html')
@@ -12,4 +22,16 @@ def history(user_id):
         return render_template('history.html', executables=user['executables'])
     else:
         return "Invalid user", 401
+
+@app.route("/test")
+@oauth2.required
+def test()
+    return "Test oauth2", 200
  
+ 
+def _request_user_info(creds):
+    print("credentials: " + str(creds))
+
+
+
+

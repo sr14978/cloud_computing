71661a385db332d8edcea55f2571dfd4264f4b3f
Samuel Russell <sr14978@bristol.ac.uk>
2018-01-04 01:16:56 +0000

    wrap in compiler return values

--- preprocessor.py
+++ preprocessor.py
@@ -1,10 +1,13 @@
 import subprocess
+import pickle
+from compiler import Success
+from compiler import Error
+
 
 def preprocess(source_path, out_path, compiler_name='g++', flags=''):
     print("preprocessing " + source_path + " to " + out_path + " using " + compiler_name)
     cargs = [compiler_name, "-E", source_path, "-o", out_path] + flags.split(" ")
     try:
-        subprocess.check_output([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)
-        return True, None
+        return True, pickle.dumps(Success(subprocess.check_output([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)))
     except subprocess.CalledProcessError as e:
-        return False, e.output
+        return False, pickle.dumps(Error(e.output))

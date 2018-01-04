3093bd54fa7c759d946d4f5a81643e8541c8c918
Samuel Russell <sr14978@bristol.ac.uk>
2018-01-04 11:19:43 +0000

    removed print

--- linker.py
+++ linker.py
@@ -8,8 +8,6 @@ from itertools import izip_longest as zip_longest
 
 class Result:
     def __add__(self, msgs):
-        msgs = [msg.decode("utf-8") for msg in msgs]
-        print("linker.Result.__add__: self.msgs=" + ",".join(self.msgs) + " msgs= " + ",".join(msgs))
         self.msgs = msgs + self.msgs
         return self
 

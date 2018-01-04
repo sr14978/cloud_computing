22ea806df0f8e82267d5d8de96bc8a9fe165c94a
Samuel Russell <sr14978@bristol.ac.uk>
2018-01-04 01:02:13 +0000

    check output

--- preprocessor.py
+++ preprocessor.py
@@ -4,7 +4,7 @@ def preprocess(source_path, out_path, compiler_name='g++', flags=''):
     print("preprocessing " + source_path + " to " + out_path + " using " + compiler_name)
     cargs = [compiler_name, "-E", source_path, "-o", out_path] + flags.split(" ")
     try:
-        subprocess.call([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)
+        subprocess.check_output([arg for arg in cargs if arg != ""], stderr=subprocess.STDOUT)
         return True, None
     except subprocess.CalledProcessError as e:
         return False, e.output

bbb2f3cb6514d465325178aba164f7390c111f86
Samuel Russell <sr14978@bristol.ac.uk>
2018-01-04 20:38:01 +0000

    slide up down

--- static/scripts/history.js
+++ static/scripts/history.js
@@ -8,29 +8,36 @@ function onLoad(e) {
   
   function displyMessage(e) {
     e.preventDefault();
-    var spinner = $(e.target).parent().children('div.spinner')
-    spinner.toggleClass('invisible')
-    var url = $(e.target).parent().children('input.message-url').get(0).value
     var text_box = $(e.target).parent().parent().children('div.item-message')
-    $.ajax({
-      dataType: "json",
-      url: url,
-      success: function(data) {
-        
-        var str = data['messages'].join("\n")
-        str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
-        text_box.html(str)
-        
-        spinner.toggleClass('invisible')
-        text_box.slideToggle()
-      },
-      error: function() {
-        $('#compiling_spinner').hide();
-        alert("could not get results")
-        spinner.toggleClass('invisible')
-      }
-    });
-    
+    if(text_box.is(':visible')) {
+      text_box.slideToggle()
+    } else {
+      var spinner = $(e.target).parent().children('div.spinner')
+      spinner.toggleClass('invisible')
+      var url = $(e.target).parent().children('input.message-url').get(0).value
+      
+      $.ajax({
+        dataType: "json",
+        url: url,
+        success: function(data) {
+          
+          var str = data['messages'].join("\n")
+          str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
+          if(str.length == 0) {
+            str = "No errors, or warnings"
+          }
+          text_box.html(str)
+          
+          spinner.toggleClass('invisible')
+          text_box.slideToggle()
+        },
+        error: function() {
+          $('#compiling_spinner').hide();
+          alert("could not get results")
+          spinner.toggleClass('invisible')
+        }
+      });
+    }
     
   }
 }

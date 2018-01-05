eabb29b017874854bd29fa455ff90e865479ac33
Samuel Russell <sr14978@bristol.ac.uk>
2018-01-05 15:53:02 +0000

    increased ask deadline

--- queue.py
+++ queue.py
@@ -15,6 +15,7 @@ def get_publisher(name):
   sub_name = subscriber_client.subscription_path('cloudcomputingcompliler', name + '_jobs')
   push_config = PushConfig()
   push_config.push_endpoint = 'https://worker-dot-cloudcomputingcompliler.appspot.com/_ah/' + name + '/'
+  push_config.ack_deadline_seconds = 30
   try:
     subscription = subscriber_client.create_subscription(sub_name, topic_name, push_config)
   except AlreadyExists:
